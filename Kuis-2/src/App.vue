dfab2581286fef3546290ba9a89e1a171223ba2e
amalina30 <amalina3037@gmail.com>
2023-06-06 22:25:15 +0700

    first commit

---
+++ Kuis-2/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from './components/Navbar.vue';
+import NavBar2 from './components/Navbar2.vue';
+import NavBar3 from './components/NavBar3.vue';
+</script>
+
+<template>
+  <header>
+    <NavBar />
+  </header>
+  <NavBar2 />
+  <NavBar3 />
+</template>
